279f86b0ff30336ac0f6819e754d275ba644509d
barelybear <nghiem.trinhaman@hcmut.edu.vn>
2025-06-07 16:17:41 +0700

    update board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,59 +1,59 @@
-//using Unity.VisualScripting;
-//using UnityEngine;
-//using UnityEngine.Tilemaps;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
-//public class Board : MonoBehaviour
-//{
-//    public Tilemap tilemap { get; private set; }
-//    public Tile tileEmpty;
-//    public Tile tileExploded;
-//    public Tile tileFlag;
-//    public Tile tileMine;
-//    public Tile tileUnknown;
-//    public Tile[] tileNumber = new Tile[9];
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public TileBase tileEmpty;
+    public TileBase tileExploded;
+    public TileBase tileFlag;
+    public TileBase tileMine;
+    public TileBase tileUnknown;
+    public TileBase[] tileNumber = new TileBase[9];
 
 
-//    private void Awake()
-//    {
-//        tilemap = GetComponent<Tilemap>();
-//    }
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
 
-//    private Tile getTile(CellData cell)
-//    {
-//        if (cell.IsRevealed)
-//        {
-//            if (cell.CellType == CellData.Type.Number)
-//            {
-//                return tileNumber[cell.Number];
-//            }
-//            else if (cell.CellType == CellData.Type.Empty)
-//            {
-//                return tileEmpty;
-//            }
-//            else
-//            {
-//                return tileExploded;
-//            }
-//        }
-//        else if (cell.IsFlag)
-//        {
-//            return tileFlag;
-//        }
+    private TileBase getTile(CellData cell)
+    {
+        if (cell.isRevealed)
+        {
+            if (cell.cellType == CellData.Type.Number)
+            {
+                return tileNumber[cell.number];
+            }
+            else if (cell.cellType == CellData.Type.Empty)
+            {
+                return tileEmpty;
+            }
+            else
+            {
+                return tileExploded;
+            }
+        }
+        else if (cell.isFlag)
+        {
+            return tileFlag;
+        }
 
-//        return tileUnknown;
-//    }
-//    public void Draw(CellData[,] state)
-//    {
-//        int width = state.GetLength(0);
-//        int height = state.GetLength(0);
+        return tileUnknown;
+    }
+    public void Draw(CellData[,] state)
+    {
+        int width = state.GetLength(0);
+        int height = state.GetLength(0);
 
-//        for (int i = 0; i < width; i++)
-//        {
-//            for (int j = 0; j < height; j++)
-//            {
-//                CellData cell = state[i, j];
-//                tilemap.SetTile(cell.position, getTile(cell));
-//            }
-//        }
-//    }
-//}
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                CellData cell = state[i, j];
+                tilemap.SetTile(cell.position, getTile(cell));
+            }
+        }
+    }
+}
